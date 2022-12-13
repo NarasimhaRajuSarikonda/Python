d4274400e23bf100c0c811174475ff48c20251a0
NarasimhaRajuSarikonda <narasimhasarikonda8964@gmail.com>
2022-12-13 23:43:48 +0530

    code

---
+++ Printing_star_pattern_rev.py
@@ -0,0 +1,5 @@
+a=int(input("Enter number of lines:"))
+for i in range(a,0,-1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print(" ")
---
+++ printing_hello_n_times_for-loop.py
@@ -0,0 +1,3 @@
+a=int(input("how many times hello should be printed?"))
+for i in range(a):
+    print("hello")
